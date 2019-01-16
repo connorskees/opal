d79e98bf290cd5710776eabf19ece0766d111afd
Connor Skees <39542938+ConnorSkees@users.noreply.github.com>
2019-01-16 11:29:16 -0500

    Add more manual edits to meal

--- opal.py
+++ opal.py
@@ -459,6 +459,9 @@ class Opal:
             "w/": "with", # This abbreviation is annoying
             "Pasta and Meat Sauce": "Pasta with Meat Sauce",
             "Salisbury Steak and Bread": "Salisbury Steak with Bread",
+            "Nachos Waffle Cone": "Nachos in a Waffle Cone,
+            "Turkey Bacon Club Pretzel Melt": "Turkey Bacon Pretzel Melt",
+            
             "No School-": "No School:",
             "No School for": "No School:",
             "New Year's Day - No School": "No School: New Year's Day",
@@ -471,6 +474,7 @@ class Opal:
             "Holiday Break - No School": "No School: Holiday Break",
             "Early Dismissal-No Lunch": "Early Dismissal",
             "Early Dismissal - No Lunch": "Early Dismissal",
+            "No School-Faculty In-service": "No School: Faculty In-Service",
         }
 
         for original, replacement in replacements.items():

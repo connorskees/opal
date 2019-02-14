34f03812ce4593ee386942b7d9f6ad539d20b31b
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-02-13 21:33:13 -0500

    Add several adjectives

--- opal.py
+++ opal.py
@@ -204,7 +204,9 @@ class Opal:
             'Seagull', 'Busy', 'British', 'Curdled',
             'Delicious!', 'Student at Harvard', 'Malicious', 'NSFW',
             'Suspicious', 'Tainted', 'Cold', 'Mushy', 'Youere\'r Mom\'s',
-            'Byzantine', 'Ionian'
+            'Byzantine', 'Ionian', 'Lousy', 'Ungovernable',
+            'Sad :(', 'Raw', 'Uncooked', 'Boiled', 'Republican',
+            'Ted Cruz\'s'
         )
 
         self.emails_dict = {

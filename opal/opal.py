49c60bd38c47cea278bdd0665c2f7ed0e84b3dfa
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-05-25 20:01:36 -0400

    Remove --no-sandbox flag from headless chrome

--- opal/opal.py
+++ opal/opal.py
@@ -176,7 +176,6 @@ class Opal:
         if not self.gui:
             options.add_argument('--headless')
             options.add_argument('--disable-gpu')
-            options.add_argument('--no-sandbox')
             options.add_argument('--window-size=1280,800')
             options.add_argument('--log-level=3')
 
